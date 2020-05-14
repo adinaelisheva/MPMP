73f7a78076ccdce41bf5efd55a88760e9274a764
Adina Rubinoff <adina.elisheva@gmail.com>
2020-05-14 01:00:28 -0400

    final version + readme

---
+++ bank-6/README.md
@@ -0,0 +1,12 @@
+### Puzzle number 6
+#### The Million Bank Balance Puzzle
+
+See https://www.think-maths.co.uk/BankBalance
+
+The question: you start with 2 deposits on days 1 and 2. Subsequently, a fibonacci-style increasing function is applied each day. What 2 deposits take the LONGEST time to hit exactly $1000000 in the bank?
+
+I just brute forced this. My original attempt took about an hour, but with a simple short-circuit, it takes about 0.9s!
+
+```> node bank.js```
+
+Process: Originally I ran this pure brute force, looping i and j both to a million. This took a long time, 4689.99s, or 78.1665 minutes, or 1.3 hours. I noticed the correct answer was in the first 0.001% of the search, after 1.624 seconds. So it felt like the remaining 78.14 minutes of work wasn't necessary. Was it? The smaller your first deposit is, the smaller your steps are, increasing the numbers of days to win. And the smaller your second deposit is, the farther from your goal you start, also increasing the number of days. It's not possible to beat the current best if both first AND second deposits are larger than your current best - and this gave me a method of short circuiting.
--- bank-6/bank.js
+++ bank-6/bank.js
@@ -1,42 +1,43 @@
 let bestLen = 0;
 let best = [];
 
-let goalAmt = 1000000;
+const goalAmt = 1000000;
 
 function tryDeposit(a, b) {
   b = a+b;
   let sum = 0;
-  let c = 2; // a and b count for the first 2 days
-  for (; sum < goalAmt; c++) {
+  let days = 2; // a and b count for the first 2 days
+  for (; sum < goalAmt; days++) {
     sum = a + b;
     [a,b] = [b,sum];
   }
   if (sum === goalAmt) {
-    return c;
+    return days;
   }
   return -1;
 }
 
-const startTime = new Date();
-let oldPctStr = '';
+const startTime = new Date(); // to log how long this takes to run
+
 // Note: if i is half the goal, this will never work, as i + (i + j) will be over the goal no matter what j is
 for (let i = 1; i < goalAmt/2; i++) {
-  let pct = ((i-1) / (goalAmt/2)) * 100;
-  let pctStr = pct.toFixed(3);
-  if (pctStr !== oldPctStr) {
-    console.warn(`${pctStr}% (${i})`);
-    oldPctStr = pctStr;
-  }
 
-  for (let j = 1; j < goalAmt; j++) {
+  // Short circuiting - if both i and j are above your current best attempt, there's no way they will beat it. 
+  // So only run j up to the current best if i is already over it.
+  let jLimit = goalAmt;
+  if (i > best[0]) {
+    jLimit = best[1];
+  }
+  
+  for (let j = 1; j < jLimit; j++) {
     const res = tryDeposit(i,j);
     if (res > bestLen) {
       bestLen = res;
       best = [i,j];
-      console.log(`${i}, ${j} is better - reaches \$${goalAmt} in ${bestLen} days`);
-      console.warn(`${i}, ${j} is better - reaches \$${goalAmt} in ${bestLen} days`);
+      console.log(`[${i}, ${j}] is better - ${bestLen} days`);
     }
   }
 }
 const endTime = new Date();
 console.log('\n'+(endTime.getTime() - startTime.getTime())/1000 + 's');
+console.log(`Best starting deposits are ${best[0]} and ${best[1]} which take ${bestLen} days to reach \$${goalAmt}`);
